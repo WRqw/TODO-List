4ac2b8e1a829e942870c9bd4aa6278524297dbe2
WRqw <daniil.korshackov@yandex.ru>
2015-09-04 00:20:00 +0300

    Starting study
    Add html site, announced an array, prompt func, add while cycle, and if
    for null values.

---
+++ todo.js
@@ -0,0 +1,21 @@
+
+
+var mass = [];
+
+while (true) {
+
+	var value = prompt('Enter event');
+
+	mass.push(value);
+
+	if(value == 0 || value == undefined || value == null){
+		break;
+	}
+}
+
+
+
+
+alert(mass);
+
+
