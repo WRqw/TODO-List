d424910a3a9ed9391e6f797079739deace6a8bbf
WRqw <daniil.korshackov@yandex.ru>
2015-09-04 15:40:06 +0300

    todo is working
    function of create string, fonts

--- todo.js
+++ todo.js
@@ -7,15 +7,35 @@ while (true) {
 	var value = prompt('Enter event');
 
 	mass.push(value);
+	
+	
 
 	if(value == 0 || value == undefined || value == null){
 		break;
 	}
+	
+}
+for(var i = mass.length - 1; i >= 0; i--){
+	if(mass[i] == ","){
+		mass[i] = "";
+	}
 }
 
+document.body.innerHTML = '<div class="wrapper">' + mass.map(function(todo) {
+
+
+ return '<p class= "todo">' + todo + '</p>';
+})
 
 
 
-alert(mass);
+
+
+
+
+
+
+
+
 
 
